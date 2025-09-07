efe6df48bda06355dc34369158637cdb60343693
yying8 <yueyan.ying0256@gmail.com>
2025-09-06 22:46:58 -0400

    save

--- src/YingYechang/yycTextPart.tsx
+++ src/YingYechang/yycTextPart.tsx
@@ -78,7 +78,7 @@ function YycTextPart()
         </div>
         {/* 默认1920宽 */}
         <img src="/picture/yingyechang/yyc_1.png" alt="" style={{ transform: "translateX(-490px) rotate(10deg) scale(1.5)", marginTop: "180px" }} />
-        <p style={{ color: "var(--font-color-orange)", transform: " rotate(10deg) translateX(400px) translateY(-520px)" }}>
+        <p style={{ color: "var(--font-color-orange)", transform: " rotate(10deg) translateX(350px) translateY(-500px)" }}>
           Size: 142 × 210 mm<br />
           Page count: 304 pages<br />
           First edition: 200 copies<br />
